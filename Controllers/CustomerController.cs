9e160c2acef2a31f1075245cd09adcdb8dd56288
Đỗ Nguyễn Huy Hoàng <50350403+hoanghip108@users.noreply.github.com>
2022-10-22 12:15:02 +0700

    hoang2

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -2,6 +2,7 @@
 using NotUseAuto.Data;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace NotUseAuto.Controllers
 {
@@ -19,6 +20,15 @@ namespace NotUseAuto.Controllers
             var categories = context.Category.ToList();
             ViewBag.Categories = categories;
             return View(products);
+        }
+        public IActionResult Index2(int? id)
+        {
+            var products = context.Product.ToList();
+            var categories = context.Category.ToList();
+            ViewBag.Categories = categories;
+            var productSearch = context.Category.Include(c => c.Products).FirstOrDefault(c => c.Id == id);
+
+            return View(productSearch);
         }
     }
 }
